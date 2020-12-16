a107ccde9dae590575f3bfc923c380061db77a7c
Danilo Lustosa <danilolustosa@gmail.com>
2020-12-16 13:40:44 -0300

    header http rest

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Pokemon } from './../models/pokemon.model';
 import { PokemonList } from '../models/pokemon-list.model';
 
 const apiURL = "https://pokeapi.co/api/v2/";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization':  'Bearer test_a8c37778328495ac24c5d0d3c3923b'
+  }),
+  params: new HttpParams().set('limit', '20')
+};
+
 @Injectable()
 export class PokemonService {
+
+
 
   constructor(private http:HttpClient) { }
 
