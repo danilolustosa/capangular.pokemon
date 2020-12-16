e93d839621c1fe1e0a83caad4cd8077cc0541578
Danilo Lustosa <danilolustosa@gmail.com>
2020-12-16 13:19:09 -0300

    router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 const routes: Routes = [];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,62 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Pokemon } from './models/pokemon.model';
 import { PokemonService } from './services/pokemon.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon';
   pokemon:Pokemon = {};
   error:boolean = false;
   errorMessage:string = "";
 
-  constructor(private service:PokemonService) { }
+  constructor(private service:PokemonService,private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(){
+    this.activatedRoute.queryParams.subscribe(params => {
+      const nomePokemon = params['nome'];
+
+      if (nomePokemon){
+        this.getPokemon(nomePokemon);
+      }
+
+    });
+  }
 
   onSubmit(form:NgForm){
+
     //type Form = { nome:string }
     //let formValue:Form = form.value;
 
+
     if (form.valid){
-      this.service.getPokemon(form.value.nome)
-      .subscribe((response:any) => {
-        this.error = false;
-        this.pokemon = response;
-        console.log(this.pokemon);
-      }, (error:HttpErrorResponse) => {
-        if (error.status == 404) {
-          this.error = true;
-          this.errorMessage = "Pokémon não encontrado";
-        }
-      });
+      this.getPokemon(form.value.nome);
     } else {
       this.error = true;
       this.errorMessage = "Nome do Pokémon não informado";
     }
 
 
+  }
+
+  getPokemon(nome:string){
+    this.service.getPokemon(nome)
+    .subscribe((response:any) => {
+      this.error = false;
+      this.pokemon = response;
+      console.log(this.pokemon);
+    }, (error:HttpErrorResponse) => {
+      if (error.status == 404) {
+        this.error = true;
+        this.errorMessage = "Pokémon não encontrado";
+      }
+    });
   }
 
 
